7b698973847ab576de7a254211e1d4a0a2b42d41
Zach <40471358+orrefailaT@users.noreply.github.com>
2023-12-01 21:56:36 -0500

    2021.1

--- 2021/1/day1.py
+++ 2021/1/day1.py
@@ -1,13 +1,10 @@
-import os
-
 def main():
     file = open("1/input", "r")
     lines = file.readlines()
 
     part_one(lines)
     part_two(lines)
-    
-    
+
 
 def part_one(lines):
     count = 0
@@ -21,17 +18,15 @@ def part_one(lines):
 
     print(count)
 
+
 def part_two(lines):
     count = 0
 
-    for i in range (1, len(lines) - 1):
+    for i in range(1, len(lines) - 1):
         if window_sum(lines, i) > window_sum(lines, i - 1):
             count += 1
 
     print(count)
-
-
-
 
 
 def window_sum(lines, i):
@@ -45,6 +40,7 @@ def window_sum(lines, i):
 def line_to_num(line):
     num_str = line[0:-1]
     return int(num_str)
+
 
 if __name__ == "__main__":
     main()
