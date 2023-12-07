c804a7b40d05dc6426c6a485ffa7af10e1022733
Zach <40471358+orrefailaT@users.noreply.github.com>
2023-12-06 23:54:25 -0500

    2023 day 1 part 2

--- rust/2023/src/bin/1.rs
+++ rust/2023/src/bin/1.rs
@@ -13,15 +13,34 @@ fn part_one(input: &str) {
 }
 
 fn part_two(input: &str) {
+    let numbers = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
     let mut sum: u32 = 0;
 
     for line in input.lines() {
-        let first: char = line.chars().find(|c| c.is_ascii_digit()).unwrap();
-        let last: char = line.chars().rfind(|c| c.is_ascii_digit()).unwrap();
-        sum += format!("{first}{last}").parse::<u32>().unwrap();
+        let mut first: u32 = 0;
+        let mut last: u32 = 0;
+        for (i, c) in line.chars().enumerate() {
+            if c.is_ascii_digit() {
+                if first == 0 {
+                    first = c.to_digit(10).unwrap();
+                }
+                last = c.to_digit(10).unwrap();
+            } else if let Some((j, _)) = numbers.iter().enumerate().find(|(_, num)| {
+                let end = i + num.len();
+                end - 1 < line.len() && ***num == line[i..end]
+            }) {
+                if first == 0 {
+                    first = j as u32 + 1;
+                }
+                last = j as u32 + 1;
+            }
+        }
+        sum += 10 * first + last;
     }
 
-    println!("Part One Answer: {}", sum)
+    println!("Part Two Answer: {}", sum)
 }
 
 fn main() {
